88b65afb6d7663b199981ccf6b6bf5a734b7fc2b
Bulelani Botman <69175349+blkniga@users.noreply.github.com>
2022-08-11 02:23:30 +0200

    Fixed some issues with the code

--- get_width.c
+++ get_width.c
@@ -3,10 +3,10 @@
 /**
  * get_width - Calculates the width for printing
  * @format: Formatted string in which to print the arguments.
- * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * @i: Arguments to be printed
+ * @list: list of arguments
  *
- * Return: width.
+ * Return: width
  */
 int get_width(const char *format, int *i, va_list list)
 {
@@ -24,6 +24,11 @@ int get_width(const char *format, int *i, va_list list)
 		{
 			curr_i++;
 			width = va_arg(list, int);
+			break;
+		}
+		else
+		{
+			break;
 		}
 	}
 
